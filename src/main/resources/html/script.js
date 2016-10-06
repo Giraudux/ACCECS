f2beff8d3de69014dc84fd570ccb8f2cdef93187
Leo <leo.cassiau@etu.univ-nantes.fr>
2016-10-06 15:51:00 +0200

    correction du bug qui f'sait que quand on ajoutait des variables, on reinitialisait les variables déjà ajoutés.

--- src/main/resources/html/script.js
+++ src/main/resources/html/script.js
@@ -1,19 +1,21 @@
 var cptVar=0;
 
 function printFormVariable() {
-	return "<div id=\"variable" + cptVar + "\">" +
-	"<label> var : </label> <textarea rows=1 name=\"variableName" + cptVar + "\"> name </textarea>" +
+	return "<label> var : </label> <textarea rows=1 name=\"variableName" + cptVar + "\"> name </textarea>" +
 	"<label> type : </label> <select name=\"variableType" + cptVar + "\"> <option> Integer </option> <option> Float </option> <option> Boolean </option>  </select> " +
 	"<label> role : </label> <select name=\"variableRole" + cptVar + "\"> <option> input </option> <option> output </option> <option> control </option>  <option> internal </option> </select>" +
 	"<label> Lower bound : </label> <textarea name=\"variableMin" + cptVar + "\">  </textarea>"+ 
 	"<label> Upper bound : </label> <textarea name=\"variableMax" + cptVar + "\">  </textarea>" +
 	"<label> Default value (optional) </label> <textarea  name=\"variableInit" + cptVar + "\"> </textarea>" +
-	"<br/> </div>";
+	"<br/> ";
 
 }
 
 function newVariable(){
-	document.getElementById("newvariables").innerHTML += printFormVariable();
+  var div = document.createElement('div');
+  div.id = "variable" + cptVar;
+  div.innerHTML = printFormVariable();
+	document.getElementById("newvariables").appendChild(div);
 	cptVar++; 
 }
 
