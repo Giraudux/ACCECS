acf652a1d550a633707693e1a6bb92440026cb6f
Leo <leo.cassiau@etu.univ-nantes.fr>
2016-10-13 15:23:04 +0200

    Correction du bug des doubles guillemets dans le JSON

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,12 +1,12 @@
 var cptVar=0;
 
 function printFormVariable() {
-	return "<label> var : </label> <textarea rows=1 class=\"variableName\"> name </textarea>" +
+	return "<label> var : </label> <textarea rows=1 class=\"variableName\">name</textarea>" +
 	"<label> type : </label> <select class=\"variableType\"> <option> Integer </option> <option> Float </option> <option> Boolean </option>  </select> " +
 	"<label> role : </label> <select class=\"variableRole\"> <option> input </option> <option> output </option> <option> control </option>  <option> internal </option> </select>" +
-	"<label> Lower bound : </label> <textarea class=\"variableMin\">  </textarea>"+ 
-	"<label> Upper bound : </label> <textarea class=\"variableMax\">  </textarea>" +
-	"<label> Default value (optional) </label> <textarea  class=\"variableInit\"> </textarea>" + 
+	"<label> Lower bound : </label> <textarea class=\"variableMin\"></textarea>"+ 
+	"<label> Upper bound : </label> <textarea class=\"variableMax\"></textarea>" +
+	"<label> Default value (optional) </label> <textarea  class=\"variableInit\"></textarea>" + 
   "<input type=\"button\" value=\"DELETE\" onclick=\"removeVariable( " + cptVar  +")\">" +
 	"<br/> ";
 
@@ -35,22 +35,24 @@ function generateModelJSON(){
   var data = { variables : [], properties : ""};
   for(i = 0 ; i<document.getElementsByClassName("variableName").length ; i++) {
     var variable = {};
-    variable.variableName =  JSON.stringify(document.getElementsByClassName("variableName")[i].value);
-    variable.variableType =  JSON.stringify(document.getElementsByClassName("variableType")[i].value);
-    variable.variableRole =  JSON.stringify(document.getElementsByClassName("variableRole")[i].value);
-    variable.variableMin =  JSON.stringify(document.getElementsByClassName("variableMin")[i].value);
-    variable.variableMax =  JSON.stringify(document.getElementsByClassName("variableMax")[i].value);
-    variable.variableInit =  JSON.stringify(document.getElementsByClassName("variableInit")[i].value);
+    variable.variableName =  document.getElementsByClassName("variableName")[i].value;
+    variable.variableType =  document.getElementsByClassName("variableType")[i].value;
+    variable.variableRole =  document.getElementsByClassName("variableRole")[i].value;
+    variable.variableMin =  document.getElementsByClassName("variableMin")[i].value;
+    variable.variableMax =  document.getElementsByClassName("variableMax")[i].value;
+    variable.variableInit =  document.getElementsByClassName("variableInit")[i].value;
 
     data.variables.push(variable);
   }
 
-  data.properties = JSON.stringify(document.getElementsByClassName("textAreaProperties")[0].value);
-	/*
+  data.properties = document.getElementsByClassName("textAreaProperties")[0].value;
+	
+	console.log(data);
 	console.log(data.variables[0]);
 	console.log(data.variables[1]);
 	console.log(data.properties);
-	*/
+	
 	sendJSON(data);
 }
+
 
