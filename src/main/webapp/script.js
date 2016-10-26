1eacf14f67ada3df9c4229195af23fe86ea22823
Giraudux <giraudux@gmail.com>
2016-10-27 01:34:59 +0200

    new arch

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,90 +1,161 @@
-var cptVar=0;
-function printFormVariable() {
-	return "<fieldset>" +
-	"<label> name: </label> <input type=\"text\" class=\"variableName\"> <br>" +
-	"<label> type: </label> <select id=\"selectType" + cptVar +"\" class=\"variableType\" onchange=\"checkType(" + cptVar  +")\"> <option> Integer </option> <option> Float </option> <option> Boolean </option>  </select> <br>" +
-	"<label> role: </label> <select class=\"variableRole\"> <option> input </option> <option> output </option> <option> control </option>  <option> internal </option> </select> <br>" +
-	"<label id=\"labelLowerBound" + cptVar +"\"> Lower bound : </label> <input type=\"text\" id=\"lowerBound" + cptVar +"\" class=\"variableMin\"> <br>"+
-	"<label id=\"labelUpperBound" + cptVar +"\"> Upper bound : </label> <input type=\"text\" id=\"upperBound" + cptVar +"\" class=\"variableMax\"> <br>" +
-	"<label> Default value (optional) </label> <input type=\"text\" id=\"defaultValue" + cptVar +"\" class=\"variableInit\"> <br>" +
-    "<input id=\"delete" + cptVar +"\" type=\"button\" value=\"DELETE\" onclick=\"removeVariable( " + cptVar  +")\">" +
-	"</fieldset>" +
-	"<br/> ";
 
+/**
+ *
+ */
+function newElement(name, attributes, childs) {
+    var element;
+    var attribute;
+    var i;
+
+    element = document.createElement(name);
+
+    for(attribute in attributes) {
+        if(attributes.hasOwnProperty(attribute)) {
+            element.setAttribute(attribute, attributes[attribute]);
+        }
+    }
+
+    for(i = 0; i < childs.length; i++) {
+        element.appendChild(childs[i]);
+    }
+
+    return element;
 }
 
-function newVariable(){
-  var div = document.createElement('div');
-  div.id = "variable" + cptVar;
-  div.innerHTML = printFormVariable();
-	document.getElementById("variables").appendChild(div);
-	cptVar++; 
+/**
+ *
+ */
+function newElementProperty() {
+    return newElement("FIELDSET", {class: "Property"}, [
+        newElement("BUTTON", {type: "button", onclick: "this.parentNode.parentNode.removeChild(this.parentNode)"}, [document.createTextNode("Remove")]),
+        newElement("INPUT", {type: "text", class: "PropertyExpression"}, [])
+    ]);
 }
 
-function removeVariable(rankVar){
-	document.getElementById("variables").removeChild(document.getElementById("variable" + rankVar));
+/**
+ *
+ */
+function newElementVariable() {
+    var variable;
+    var element;
+    var option;
+
+    variable = newElement("FIELDSET", {class: "Variable"}, []);
+
+    variable.appendChild(newElement("BUTTON", {type: "button", onclick: "this.parentNode.parentNode.removeChild(this.parentNode)"}, [document.createTextNode("Remove")]));
+
+    variable.appendChild(newElement("LABEL", {}, [document.createTextNode("Name:")]));
+    variable.appendChild(newElement("INPUT", {type: "text", class: "VariableName"}, []));
+
+    variable.appendChild(newElement("LABEL", {}, [document.createTextNode("Type:")]));
+    variable.appendChild(newElement("SELECT", {class: "VariableType", onchange: "updateVariable(this.parentNode)"}, [
+        newElement("OPTION", {value: "integer"}, [document.createTextNode("Integer")]),
+        newElement("OPTION", {value: "float"}, [document.createTextNode("Float")]),
+        newElement("OPTION", {value: "boolean"}, [document.createTextNode("Boolean")])
+    ]));
+
+    variable.appendChild(newElement("LABEL", {}, [document.createTextNode("Category:")]));
+    variable.appendChild(newElement("SELECT", {class: "VariableCategory"}, [
+        newElement("OPTION", {value: "input"}, [document.createTextNode("Input")]),
+        newElement("OPTION", {value: "output"}, [document.createTextNode("Output")]),
+        newElement("OPTION", {value: "control"}, [document.createTextNode("Control")]),
+        newElement("OPTION", {value: "internal"}, [document.createTextNode("Internal")])
+    ]));
+
+    variable.appendChild(newElement("LABEL", {}, [document.createTextNode("Lower bound:")]));
+    variable.appendChild(newElement("INPUT", {type: "number", class: "VariableLowerBound"/*, step: "any"*/}, []));
+
+    variable.appendChild(newElement("LABEL", {}, [document.createTextNode("Upper bound:")]));
+    variable.appendChild(newElement("INPUT", {type: "number", class: "VariableUpperBound"/*, step: "any"*/}, []));
+
+    variable.appendChild(newElement("LABEL", {}, [document.createTextNode("Default value:")]));
+    variable.appendChild(newElement("INPUT", {type: "text", class: "VariableDefaultValue"}, []));
+
+    return variable;
 }
 
-function checkType(rankVar){
-  var x = document.getElementById("selectType" + rankVar).value;
-  if (x=="Boolean"){
-    document.getElementById("lowerBound" + rankVar).style.visibility = "hidden";
-    document.getElementById("upperBound" + rankVar).style.visibility = "hidden";
-    document.getElementById("labelLowerBound" + rankVar).style.visibility = "hidden";
-    document.getElementById("labelUpperBound" + rankVar).style.visibility = "hidden";
-    document.getElementById("variable" + rankVar).removeChild(document.getElementById("defaultValue" + rankVar));
-    document.getElementById("variable" + rankVar).removeChild(document.getElementById("delete" + rankVar));
-    document.getElementById("variable" + rankVar).innerHTML+="<select id=\"defaultValue" + rankVar +"\"class=\"variableInit\"> <option> true </option> <option> false </option> </select>"+
-      "<input id=\"delete" + rankVar +"\" type=\"button\" value=\"DELETE\" onclick=\"removeVariable( " + rankVar  +")\">";
-    document.getElementById("selectType" + rankVar).value=x;
-    
-    } else{
-      document.getElementById("lowerBound" + rankVar).style.visibility = "visible";
-      document.getElementById("upperBound" + rankVar).style.visibility = "visible";
-      document.getElementById("labelLowerBound" + rankVar).style.visibility = "visible";
-      document.getElementById("labelUpperBound" + rankVar).style.visibility = "visible";
-      document.getElementById("variable" + rankVar).removeChild(document.getElementById("defaultValue" + rankVar));
-      document.getElementById("variable" + rankVar).removeChild(document.getElementById("delete" + rankVar));
-      document.getElementById("variable" + rankVar).innerHTML+="<textarea id=\"defaultValue" + rankVar +"\" class=\"variableInit\"></textarea>"+
-      "<input id=\"delete" + rankVar +"\" type=\"button\" value=\"DELETE\" onclick=\"removeVariable( " + rankVar  +")\">";
-      document.getElementById("selectType" + rankVar).value=x;
+/**
+ *
+ */
+function updateVariable(variable) {
+    var type;
+    var disableBounds;
+    var step = "any";
+
+    type = variable.getElementsByClassName("VariableType")[0].value;
+    variable.removeChild(variable.getElementsByClassName("VariableDefaultValue")[0]);
+
+    if(type == "boolean") {
+         disableBounds = true;
+         variable.appendChild(newElement("SELECT", {class: "VariableDefaultValue"}, [
+             newElement("OPTION", {value: "true"}, [document.createTextNode("True")]),
+             newElement("OPTION", {value: "false"}, [document.createTextNode("False")])
+         ]));
+    } else {
+        disableBounds = false;
+        if(type == "integer") {
+            step = "1";
+        } else if(type == "float") {
+            step = "any";
+        }
+        variable.appendChild(newElement("INPUT", {type: "number", class: "VariableDefaultValue", step: step}, []));
+        variable.getElementsByClassName("VariableLowerBound")[0].setAttribute("step", step);
+        variable.getElementsByClassName("VariableUpperBound")[0].setAttribute("step", step);
+    }
+
+    variable.getElementsByClassName("VariableLowerBound")[0].disabled = disableBounds;
+    variable.getElementsByClassName("VariableUpperBound")[0].disabled = disableBounds;
+}
+
+/**
+ *
+ */
+function generateMachine() {
+  var xhttp;
+
+  console.log(machineToJSON());
+
+  xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           console.log(this.responseText);
+        }
+    };
+  xhttp.open("POST", "dataServlet", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("data="+encodeURIComponent(machineToJSON));
+}
+
+/**
+ *
+ */
+function machineToJSON(){
+  var machine;
+  var variables;
+  var variable;
+  var properties;
+  var i;
+
+  machine = {variables: [], properties: []};
+
+  variables = document.getElementsByClassName("Variable");
+  for(i = 0 ; i < variables.length ; i++) {
+    variable = {};
+    variable.name =  variables[i].getElementsByClassName("VariableName")[0].value;
+    variable.type =  variables[i].getElementsByClassName("VariableType")[0].value;
+    variable.category =  variables[i].getElementsByClassName("VariableCategory")[0].value;
+    variable.lowerBound =  variables[i].getElementsByClassName("VariableLowerBound")[0].value;
+    variable.upperBound =  variables[i].getElementsByClassName("VariableUpperBound")[0].value;
+    variable.defaultValue =  variables[i].getElementsByClassName("VariableDefaultValue")[0].value;
+    machine.variables.push(variable);
   }
-  
-}
-function sendJSON(data) {
-  //var url = "http://localhost:8080/accecs/dataServlet"
-  var url = "http://localhost:8080/dataServlet"
-  xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
- // xhr.setRequestHeader("Content-type", "application/json");
-  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send("data="+data);
-  document.getElementById("machine").innerHTML = xhr.responseText;
-}
 
-function generateModelJSON(){
-  var data = { variables : [], properties : ""};
-  for(i = 0 ; i<document.getElementsByClassName("variableName").length ; i++) {
-    var variable = {};
-    variable.variableName =  document.getElementsByClassName("variableName")[i].value;
-    variable.variableType =  document.getElementsByClassName("variableType")[i].value;
-    variable.variableRole =  document.getElementsByClassName("variableRole")[i].value;
-    variable.variableMin =  document.getElementsByClassName("variableMin")[i].value;
-    variable.variableMax =  document.getElementsByClassName("variableMax")[i].value;
-    variable.variableInit =  document.getElementsByClassName("variableInit")[i].value;
-
-    data.variables.push(variable);
+  properties = document.getElementsByClassName("Property");
+  for(i = 0; i < properties.length; i++) {
+      machine.properties.push(properties[i].getElementsByClassName("PropertyExpression")[0].value);
   }
 
-  data.properties = document.getElementsByClassName("textAreaProperties")[0].value;
+    console.log(machine);
 	
-	console.log(data);
-	console.log(data.variables[0]);
-	console.log(data.variables[1]);
-	console.log(data.properties);
-	
-	sendJSON(JSON.stringify(data));
+	return JSON.stringify(machine);
 }
-
-
-
